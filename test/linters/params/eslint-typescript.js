8d792c8dd74a16bdadb9964b0269b9d176e4097e
r007 <moninsergei@gmail.com>
2021-12-23 02:18:57 +0400

    Fix getLintParams inside of eslint typescript test

--- test/linters/params/eslint-typescript.js
+++ test/linters/params/eslint-typescript.js
@@ -11,11 +11,11 @@ function getLintParams(dir) {
 	const stdoutFile1 = `{"filePath":"${joinDoubleBackslash(
 		dir,
 		"file1.ts",
-	)}","messages":[{"ruleId":"prefer-const","severity":2,"message":"'str' is never reassigned. Use 'const' instead.","line":1,"column":5,"nodeType":"Identifier","messageId":"useConst","endLine":1,"endColumn":8,"fix":{"range":[0,18],"text":"const str = 'world';"}},{"ruleId":"no-warning-comments","severity":1,"message":"Unexpected 'todo' comment: 'TODO: Change something'.","line":5,"column":31,"nodeType":"Line","messageId":"unexpectedComment","endLine":5,"endColumn":56}],"errorCount":1,"warningCount":1,"fixableErrorCount":1,"fixableWarningCount":0,"source":"let str = 'world'; // \\"prefer-const\\" warning\\n\\nfunction main(): void {\\n\\t// \\"no-warning-comments\\" error\\n\\tconsole.log('hello ' + str); // TODO: Change something\\n}\\n\\nmain();\\n","usedDeprecatedRules":[]}`;
+	)}","messages":[{"ruleId":"prefer-const","severity":2,"message":"'str' is never reassigned. Use 'const' instead.","line":1,"column":5,"nodeType":"Identifier","messageId":"useConst","endLine":1,"endColumn":8,"fix":{"range":[0,18],"text":"const str = 'world';"}},{"ruleId":"no-console","severity":2,"message":"Unexpected console statement.","line":5,"column":2,"nodeType":"MemberExpression","messageId":"unexpected","endLine":5,"endColumn":13}],"errorCount":2,"fatalErrorCount":0,"warningCount":0,"fixableErrorCount":1,"fixableWarningCount":0,"source":"let str = 'world'; // \\"prefer-const\\" warning\\n\\nfunction main(): void {\\n\\t// \\"no-warning-comments\\" error\\n\\tconsole.log('hello ' + str); // TODO: Change something\\n}\\n\\nmain();\\n","usedDeprecatedRules":[{"ruleId":"no-negated-in-lhs","replacedBy":["no-unsafe-negation"]},{"ruleId":"jsx-a11y/label-has-for","replacedBy":[]}]}`;
 	const stdoutFile2 = `{"filePath":"${joinDoubleBackslash(
 		dir,
 		"file2.js",
-	)}","messages":[{"ruleId":"no-unused-vars","severity":2,"message":"'str' is assigned a value but never used.","line":1,"column":7,"nodeType":"Identifier","messageId":"unusedVar","endLine":1,"endColumn":10}],"errorCount":1,"warningCount":0,"fixableErrorCount":0,"fixableWarningCount":0,"source":"const str = 'Hello world'; // \\"no-unused-vars\\" error\\n","usedDeprecatedRules":[]}`;
+	)}","messages":[{"ruleId":"no-unused-vars","severity":2,"message":"'str' is assigned a value but never used.","line":1,"column":7,"nodeType":"Identifier","messageId":"unusedVar","endLine":1,"endColumn":10}],"errorCount":1,"fatalErrorCount":0,"warningCount":0,"fixableErrorCount":0,"fixableWarningCount":0,"source":"const str = 'Hello world'; // \\"no-unused-vars\\" error\\n","usedDeprecatedRules":[{"ruleId":"no-negated-in-lhs","replacedBy":["no-unsafe-negation"]},{"ruleId":"jsx-a11y/label-has-for","replacedBy":[]}]}`;
 	return {
 		// Expected output of the linting function
 		cmdOutput: {
@@ -26,20 +26,19 @@ function getLintParams(dir) {
 		// Expected output of the parsing function
 		lintResult: {
 			isSuccess: false,
-			warning: [
-				{
-					path: "file1.ts",
-					firstLine: 5,
-					lastLine: 5,
-					message: "Unexpected 'todo' comment: 'TODO: Change something' (no-warning-comments)",
-				},
-			],
+			warning: [],
 			error: [
 				{
 					path: "file1.ts",
 					firstLine: 1,
 					lastLine: 1,
 					message: "'str' is never reassigned. Use 'const' instead (prefer-const)",
+				},
+				{
+					path: "file1.ts",
+					firstLine: 5,
+					lastLine: 5,
+					message: "Unexpected console statement (no-console)",
 				},
 				{
 					path: "file2.js",
